ca99f87601f7c2c3a5feda2e4b05a5fc3c0422d8
parth5757 <psthakkar2@gmail.com>
2024-10-20 06:26:59 +0530

    tailwindcss configuration is installed

--- frontend/lekha/src/App.js
+++ frontend/lekha/src/App.js
@@ -1,41 +1,7 @@
-import { useState } from "react";
-import DeckGL from "@deck.gl/react";
-import StaticMap from "react-map-gl";
-import maplibregl from "maplibre-gl";
-
-import "maplibre-gl/dist/maplibre-gl.css";
-const App = () => {
-  const [viewState, setViewState] = useState({
-    longitude: 0,
-    latitude: 0,
-    zoom: 1,
-  });
-
+export default function App() {
   return (
-    <div>
-      <DeckGL
-        style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
-        viewState={viewState}
-        onViewStateChange={({ viewState }) => setViewState(viewState)}
-        controller={true}
-        layers={[]}
-      >
-        <StaticMap
-          mapLib={maplibregl}
-          mapStyle="https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json"
-          transformRequest={(url, resourceType) => {
-            if (url.includes("?")) {
-              url = url + "&api_key={54e515e8-7f5c-4c7f-b816-798a90f2d338}";
-            } else {
-              url = url + "?api_key={54e515e8-7f5c-4c7f-b816-798a90f2d338}";
-            }
-            return { url, resourceType };
-          }}
-        />
-      </DeckGL>
-      Lekha An best CRM software for you all need
-    </div>
-  );
-};
-
-export default App;
+    <h1 className="text-3xl font-bold underline">
+      Hi Parth
+    </h1>
+  )
+}
--- frontend/lekha/tailwind.config.js
+++ frontend/lekha/tailwind.config.js
@@ -4,28 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'base': 'Poppins, sans-serif',
-        'krona': "'Krona One', sans-serif",
-      },
-      colors: {
-        dark:"#0c080B",
-        green:"#1E3329",
-        yellow:"#FFD479",
-        'dark-grey':"#B8B8B8",
-        'light-grey':"#ECECEC",
-        light: "#FAFAFA"
-      },
-    },
+    extend: {},
   },
-  Variants: {
-    extends: {},
-  }
-
-
-  // plugins: [],
+  plugins: [],
 }
-
-
- 
